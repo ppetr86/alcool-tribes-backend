30a8a06fff44ea800a549ede168fec0396c5f991
Ahmed <ah-mady@github.com>
2021-01-23 09:35:17 +0100

    (Project_tribe) refatored

--- src/test/java/com/greenfoxacademy/springwebapp/integration_test_register_endpoint/RegistrationEndpointTest.java
+++ src/test/java/com/greenfoxacademy/springwebapp/integration_test_register_endpoint/RegistrationEndpointTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -23,8 +24,6 @@ import org.springframework.test.web.servlet.MockMvc;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-//@WebMvcTest
-//@ContextConfiguration(locations = "../resources/application.properties")
 public class RegistrationEndpointTest {
   @Autowired
   private MockMvc mockMvc;
