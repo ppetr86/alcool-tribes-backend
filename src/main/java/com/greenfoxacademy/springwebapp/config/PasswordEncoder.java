64bb3b20003d4442934914cbb32126923037d438
Ahmed <ah-mady@github.com>
2021-01-23 11:52:07 +0100

    (project_tribes) refactored: moved config file out of register package

--- src/main/java/com/greenfoxacademy/springwebapp/config/PasswordEncoder.java
+++ src/main/java/com/greenfoxacademy/springwebapp/config/PasswordEncoder.java
@@ -1,4 +1,4 @@
-package com.greenfoxacademy.springwebapp.register.config;
+package com.greenfoxacademy.springwebapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
