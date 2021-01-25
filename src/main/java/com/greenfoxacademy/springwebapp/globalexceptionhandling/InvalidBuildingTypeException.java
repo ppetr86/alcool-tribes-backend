8629639644e4d68cb20d66ec78c769f6f656d260
petr <petr@algorim.com>
2021-01-25 20:43:01 +0100

    feature: new TownhallLevelException

--- src/main/java/com/greenfoxacademy/springwebapp/globalexceptionhandling/InvalidBuildingTypeException.java
+++ src/main/java/com/greenfoxacademy/springwebapp/globalexceptionhandling/InvalidBuildingTypeException.java
@@ -3,6 +3,6 @@ package com.greenfoxacademy.springwebapp.globalexceptionhandling;
 public class InvalidBuildingTypeException extends Exception {
 
   public InvalidBuildingTypeException() {
-    super("Invalid building type || Cannot build buildings with higher level than the Townhall");
+    super("Invalid building type");
   }
 }
