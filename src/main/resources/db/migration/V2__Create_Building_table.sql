31254d8e2aa0c06d007824fc676ca731d00cc254
kelemenm <kelemen9417@gmail.com>
2021-02-01 09:40:48 +0100

    Create V2 Building table

---
+++ src/main/resources/db/migration/V2__Create_Building_table.sql
@@ -0,0 +1,8 @@
+create table Building (
+    ID int not null auto_increment,
+    TYPE varchar(50) not null,
+    LEVEL int not null ,
+    HP int not null ,
+    STARTEDAT long not null ,
+    FINISHEDAT long not null
+);
