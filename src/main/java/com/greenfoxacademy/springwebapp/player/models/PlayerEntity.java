b34b554da75775d97aa026b5a8c1fb2b7b24490e
kelemenm <kelemen9417@gmail.com>
2021-02-03 15:56:08 +0100

    put KingdomEntity into the correct package

--- src/main/java/com/greenfoxacademy/springwebapp/player/models/PlayerEntity.java
+++ src/main/java/com/greenfoxacademy/springwebapp/player/models/PlayerEntity.java
@@ -9,16 +9,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-import com.greenfoxacademy.springwebapp.buildings.models.BuildingEntity;
+import com.greenfoxacademy.springwebapp.kingdom.models.KingdomEntity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.List;
 
 @Entity
 @Getter
