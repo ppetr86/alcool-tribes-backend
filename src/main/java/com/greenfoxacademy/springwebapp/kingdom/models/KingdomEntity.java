ddd134a75dd6902757317ba775d36f0c79045260
Zdenek L <luzkix@seznam.cz>
2021-02-05 12:49:28 +0100

    refactor: kingdom entity

--- src/main/java/com/greenfoxacademy/springwebapp/kingdom/models/KingdomEntity.java
+++ src/main/java/com/greenfoxacademy/springwebapp/kingdom/models/KingdomEntity.java
@@ -1,38 +1,32 @@
 package com.greenfoxacademy.springwebapp.kingdom.models;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
 import com.greenfoxacademy.springwebapp.buildings.models.BuildingEntity;
+import com.greenfoxacademy.springwebapp.player.models.PlayerEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Table
 @Entity
-@Data
 @NoArgsConstructor
+@Data
 @AllArgsConstructor
-@Table(name = "kingdoms")
 public class KingdomEntity {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private long id;
+  private Long id;
+
+  @OneToOne
+  private PlayerEntity player;
+
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "kingdom", fetch = FetchType.EAGER)
+  private Set<BuildingEntity> buildings;
 
   @Column(name = "kingdomname")
   private String kingdomName;
 
-  @OneToMany(cascade = CascadeType.PERSIST)
-  @JoinColumn(name = "fk_buildings_kingdom")
-  private Set<BuildingEntity> setOfBuildings;
-
-  //TODO: need to add rest of fields
 }
