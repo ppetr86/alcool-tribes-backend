628bbe3b2c93f2fceb9bb7f94df9a32ccaafdd72
Ahmed <ahmad_bhai28@yahoo.com>
2021-02-20 12:31:33 +0100

    (ALTB-10) feat: added unit test for saveNewPlayer in PlayerControllerUnitTest, since previously it was mocked

--- src/test/java/com/greenfoxacademy/springwebapp/player/controllers/PlayerControllerUnitTest.java
+++ src/test/java/com/greenfoxacademy/springwebapp/player/controllers/PlayerControllerUnitTest.java
@@ -1,8 +1,17 @@
 package com.greenfoxacademy.springwebapp.player.controllers;
 
+import com.greenfoxacademy.springwebapp.building.models.BuildingEntity;
+import com.greenfoxacademy.springwebapp.building.models.enums.BuildingType;
+import com.greenfoxacademy.springwebapp.building.services.BuildingService;
+import com.greenfoxacademy.springwebapp.kingdom.models.KingdomEntity;
+import com.greenfoxacademy.springwebapp.player.models.PlayerEntity;
 import com.greenfoxacademy.springwebapp.player.models.dtos.PlayerRegistrationRequestDTO;
 import com.greenfoxacademy.springwebapp.player.models.dtos.PlayerResponseDTO;
+import com.greenfoxacademy.springwebapp.player.repositories.PlayerRepository;
 import com.greenfoxacademy.springwebapp.player.services.PlayerService;
+import com.greenfoxacademy.springwebapp.resource.models.ResourceEntity;
+import com.greenfoxacademy.springwebapp.resource.models.enums.ResourceType;
+import com.greenfoxacademy.springwebapp.resource.services.ResourceService;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,15 +21,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerControllerUnitTest {
 
   private PlayerController playerController;
   private PlayerService registrationService;
+  private BuildingService buildingService;
+  private ResourceService resourceService;
+  private PlayerRepository playerRepository;
+
 
   @Before
   public void setup() {
     registrationService = Mockito.mock(PlayerService.class);
     playerController = new PlayerController(registrationService);
+    buildingService = Mockito.mock(BuildingService.class);
+    resourceService = Mockito.mock(ResourceService.class);
+    playerRepository = Mockito.mock(PlayerRepository.class);
   }
 
   @Test
@@ -61,6 +80,32 @@ public class PlayerControllerUnitTest {
     Assert.assertEquals(1, response.getBody().getKingdomId());
     Assert.assertEquals(HttpStatus.valueOf(201), response.getStatusCode());
 
+  }
+
+  @Test
+  public void registerPlayerShouldSaveNewPlayerAndReturnCorrectResponses() {
+
+    PlayerEntity
+        fakePlayerEntity = new PlayerEntity("user1", "password");
+    PlayerRegistrationRequestDTO playerRegistrationRequestDTO =
+        new PlayerRegistrationRequestDTO("user1", "user1234", "email");
+    KingdomEntity kingdomEntity = new KingdomEntity();
+    kingdomEntity.setId(1L);
+    List<BuildingEntity> fakeListBuilding = new ArrayList<>();
+    fakeListBuilding.add(new BuildingEntity(1L, BuildingType.TOWNHALL, 1, 100, 1L, 2L, null));
+
+    List<ResourceEntity> fakeListResources = new ArrayList<>();
+    fakeListResources.add(new ResourceEntity(kingdomEntity, ResourceType.FOOD, 100, 10, 132456L));
+
+    BindingResult bindingResult = new BeanPropertyBindingResult(null, "");
+    Mockito.when(buildingService.createDefaultBuildings(kingdomEntity)).thenReturn(fakeListBuilding);
+    Mockito.when(resourceService.createDefaultResources(kingdomEntity)).thenReturn(fakeListResources);
+    Mockito.when(playerRepository.save(fakePlayerEntity)).thenReturn(fakePlayerEntity);
+
+    ResponseEntity<PlayerResponseDTO> response =
+        (ResponseEntity<PlayerResponseDTO>) playerController.registerUser(playerRegistrationRequestDTO, bindingResult);
+
+    Assert.assertEquals(HttpStatus.valueOf(201), response.getStatusCode());
   }
 
 }
