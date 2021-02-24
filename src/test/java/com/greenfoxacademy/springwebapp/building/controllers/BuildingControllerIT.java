99f13352d14ac98a64d4ddb5424b14ca80ac82dd
kelemenm <kelemen9417@gmail.com>
2021-02-24 21:33:54 +0100

    delete BuildingControllerIT2 class

--- src/test/java/com/greenfoxacademy/springwebapp/building/controllers/BuildingControllerIT.java
+++ src/test/java/com/greenfoxacademy/springwebapp/building/controllers/BuildingControllerIT.java
@@ -2,6 +2,7 @@ package com.greenfoxacademy.springwebapp.building.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.greenfoxacademy.springwebapp.TestNoSecurityConfig;
+import com.greenfoxacademy.springwebapp.building.models.dtos.BuildingLevelDTO;
 import com.greenfoxacademy.springwebapp.building.models.dtos.BuildingRequestDTO;
 import com.greenfoxacademy.springwebapp.kingdom.models.KingdomEntity;
 import com.greenfoxacademy.springwebapp.resource.services.ResourceService;
@@ -25,8 +26,8 @@ import java.util.ArrayList;
 import static com.greenfoxacademy.springwebapp.factories.AuthFactory.createAuth;
 import static com.greenfoxacademy.springwebapp.factories.BuildingFactory.createBuildings;
 import static org.hamcrest.core.Is.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @Import(TestNoSecurityConfig.class)
@@ -228,5 +229,89 @@ public class BuildingControllerIT {
             .andExpect(jsonPath("$.message", is("Cannot build buildings with higher level than the Townhall")));
   }
 
+  @Test
+  public void updateTheGivenBuildingDetailsShouldReturn404WithNoId() throws Exception {
+    BuildingLevelDTO request = new BuildingLevelDTO();
+    ObjectMapper mapper = new ObjectMapper();
+    String json = mapper.writeValueAsString(request);
 
+    mockMvc.perform(put(BuildingController.URI + "/1123")
+      .contentType(MediaType.APPLICATION_JSON)
+      .content(json)
+      .principal(authentication))
+      .andExpect(status().isNotFound())
+      .andExpect(jsonPath("$.status", is("error")))
+      .andExpect(jsonPath("$.message", is("Id not found")));
+  }
+
+  @Test
+  public void updateTheGivenBuildingDetailsShouldReturn400WithKingdomParameterMissing() throws Exception {
+    BuildingLevelDTO request = new BuildingLevelDTO();
+    ObjectMapper mapper = new ObjectMapper();
+    String json = mapper.writeValueAsString(request);
+
+    mockMvc.perform(put(BuildingController.URI + "/4")
+      .contentType(MediaType.APPLICATION_JSON)
+      .content(json)
+      .principal(authentication))
+      .andExpect(status().isBadRequest())
+      .andExpect(jsonPath("$.status", is("error")))
+      .andExpect(jsonPath("$.message", is("Missing parameter(s): level!")));
+  }
+
+  @Test
+  public void updateTheGivenBuildingDetailsShouldReturn406withTownHallNeedHigherLevel() throws Exception {
+    BuildingLevelDTO request = new BuildingLevelDTO(2);
+    ObjectMapper mapper = new ObjectMapper();
+    String json = mapper.writeValueAsString(request);
+
+    //TODO: have to remove after hasResourceForBuilding will modify
+    Mockito.when(resourceService.hasResourcesForBuilding()).thenReturn(true);
+
+    mockMvc.perform(put(BuildingController.URI + "/2")
+      .contentType(MediaType.APPLICATION_JSON)
+      .content(json)
+      .principal(authentication))
+      .andExpect(status().isNotAcceptable())
+      .andExpect(jsonPath("$.status", is("error")))
+      .andExpect(jsonPath("$.message", is("Cannot build buildings with higher level than the Townhall")));
+  }
+
+  @Test
+  public void updateTheGivenBuildingDetailsShouldReturn409WithNoResource() throws Exception {
+    BuildingLevelDTO request = new BuildingLevelDTO(2);
+    ObjectMapper mapper = new ObjectMapper();
+    String json = mapper.writeValueAsString(request);
+
+    //TODO: have to remove after hasResourceForBuilding will modify
+    Mockito.when(resourceService.hasResourcesForBuilding()).thenReturn(false);
+
+    mockMvc.perform(put(BuildingController.URI + "/1")
+      .contentType(MediaType.APPLICATION_JSON)
+      .content(json)
+      .principal(authentication))
+      .andExpect(status().isConflict())
+      .andExpect(jsonPath("$.status", is("error")))
+      .andExpect(jsonPath("$.message", is("Not enough resource")));
+  }
+
+  @Test
+  public void updateTheGivenBuildingDetailsShouldReturn200WithBuildingDetails() throws Exception {
+    BuildingLevelDTO request = new BuildingLevelDTO(3);
+    ObjectMapper mapper = new ObjectMapper();
+    String json = mapper.writeValueAsString(request);
+
+    //TODO: have to remove after hasResourceForBuilding will modify
+    Mockito.when(resourceService.hasResourcesForBuilding()).thenReturn(true);
+
+    mockMvc.perform(put(BuildingController.URI + "/1")
+      .contentType(MediaType.APPLICATION_JSON)
+      .content(json)
+      .principal(authentication))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.id", is(1)))
+      .andExpect(jsonPath("$.type", is("TOWNHALL")))
+      .andExpect(jsonPath("$.level", is(3)))
+      .andExpect(jsonPath("$.hp", is(600)));
+  }
 }
