8b618c6709f2af7ad6185a8cc2da817ff41b7519
Kond <peter.tamas@greenfoxacademy.com>
2021-02-25 13:09:06 +0100

    fix(FLyway): fix mistakes

--- src/main/resources/db/migration/V2__Create_Building_table.sql
+++
@@ -1,8 +0,0 @@
-create table buildings (
-    id bigint not null auto_increment PRIMARY KEY ,
-    type int not null,
-    level int not null ,
-    hp int not null ,
-    started_at bigint not null ,
-    finished_at bigint not null
-);
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -6,8 +6,8 @@ DELETE FROM players;
 INSERT INTO players (id, avatar, email, password, points, username)
 VALUES (1, 'http://avatar.loc/my.png', 'test@email.com', '$2a$10$NaD84OJw/IJCe6jExv21Reah0hCOgZDhp1N8D.ovKHeKtAYzevcQG',
         0, 'furkesz');
-INSERT INTO players (id, username, email, password, points)
-VALUES (100, 'occupied_username', 'test@email.com', '$2a$10$NaD84OJw/IJCe6jExv21Reah0hCOgZDhp1N8D.ovKHeKtAYzevcQG', 0);
+INSERT INTO players (id, avatar, username, email, password, points)
+VALUES (100, '', 'occupied_username', 'test@email.com', '$2a$10$NaD84OJw/IJCe6jExv21Reah0hCOgZDhp1N8D.ovKHeKtAYzevcQG', 0);
 
 INSERT INTO kingdoms (id, kingdomname, player_id)
 VALUES (1, 'furkesz''s kingdom', 1);
