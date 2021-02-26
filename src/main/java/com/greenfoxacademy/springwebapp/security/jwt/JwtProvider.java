887fe4f411d40d4135373765e80b6a32481dd810
Ahmed <ahmad_bhai28@yahoo.com>
2021-02-25 21:11:38 -0800

    remaining possible from my end

--- src/main/java/com/greenfoxacademy/springwebapp/security/jwt/JwtProvider.java
+++ src/main/java/com/greenfoxacademy/springwebapp/security/jwt/JwtProvider.java
@@ -4,17 +4,14 @@ import com.greenfoxacademy.springwebapp.player.models.PlayerEntity;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
-
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.HashMap;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
