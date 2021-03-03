a6696eb005d9d1413476fcdafdd79f8c8ae90980
petr <petr@algorim.com>
2021-03-03 14:08:14 +0100

    emaling cr changes

--- src/main/java/com/greenfoxacademy/springwebapp/player/controllers/PlayerController.java
+++ src/main/java/com/greenfoxacademy/springwebapp/player/controllers/PlayerController.java
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +23,7 @@ import javax.validation.Valid;
 @AllArgsConstructor
 public class PlayerController {
   public static final String URI = "/register";
-  public static final String URIVERIFY = "/register/verify";
+  public static final String URIVERIFY = URI + "/verify";
   private final PlayerService playerService;
 
   @GetMapping(URIVERIFY)
