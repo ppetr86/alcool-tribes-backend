5fc189ca990c38221b484ccc7fabe6f34a8ce853
Zdenek L <luzkix@seznam.cz>
2021-03-09 11:50:23 +0100

    refactor: created initial structure

---
+++ src/main/java/com/greenfoxacademy/springwebapp/battle/repositories/BattleRepository.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.springwebapp.battle.repositories;
+
+import com.greenfoxacademy.springwebapp.battle.models.BattleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BattleRepository extends JpaRepository <BattleEntity, Long> {
+
+  //repository methods to be defined here
+
+}
---
+++ src/main/java/com/greenfoxacademy/springwebapp/battle/services/BattleServiceImpl.java
@@ -0,0 +1,9 @@
+package com.greenfoxacademy.springwebapp.battle.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BattleServiceImpl implements BattleService {
+
+  //impl methods to be defined here
+}
