ff7e3796a535f7f9a6711d61b54a065265812218
Zdenek L <luzkix@seznam.cz>
2021-03-10 21:22:08 +0100

    refactor: prepared initial variables in BattleServiceImpl

--- src/main/java/com/greenfoxacademy/springwebapp/battle/services/BattleServiceImpl.java
+++ src/main/java/com/greenfoxacademy/springwebapp/battle/services/BattleServiceImpl.java
@@ -1,9 +1,28 @@
 package com.greenfoxacademy.springwebapp.battle.services;
 
+import com.greenfoxacademy.springwebapp.kingdom.models.KingdomEntity;
+import com.greenfoxacademy.springwebapp.kingdom.services.KingdomService;
+import com.greenfoxacademy.springwebapp.troop.models.TroopEntity;
+import java.util.List;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class BattleServiceImpl implements BattleService {
+  private final KingdomService kingdomService;
 
-  //impl methods to be defined here
+  private int attackerAP = 0;
+  private int attackerDP = 0;
+  private int attackerHP = 0;
+  private int defenderAP = 0;
+  private int defenderDP = 0;
+  private int defenderHP = 0;
+  private int defenderBonusDefence = 0;
+
+
+  public Boolean prepareForBattle(KingdomEntity attackingKingdom, KingdomEntity defendingKingdom,
+                                  List<TroopEntity> attackingArmy) {
+    return true;
+  }
 }
